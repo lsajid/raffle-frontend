4fc568ba8fa0ddc802e2b4aedd4d56c98ad12e85
Laiba Sajid <laibasajid@Laibas-MBP.lan>
2024-04-04 17:09:42 -0400

    add routes

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,22 @@
 import { Routes, Route } from "react-router-dom" 
 import Home from "./pages/Home"
 import NotFound from "./pages/NotFound";
+import RaffleInfo from "./pages/RaffleInfo";
 import './App.css'
+import Nav from "./components/Nav";
 
 export function App() {
   return (
-    <Routes>
-      <Route path="/" element={< Home />}/>
-      <Route path="*" element={< NotFound />}/>
-    </Routes>
+    <>
+      <Nav />
+      <Routes>
+        <Route path="/" element={< Home />}/>
+        <Route path="/raffles/:id" element={< RaffleInfo />}/>
+        <Route path="/raffles/:id/participant" element={< RaffleInfo />}/>
+        <Route path="/raffles/:id/winner" element={< RaffleInfo />}/>
+        <Route path="*" element={< NotFound />}/>
+      </Routes>
+    </>
   );
 }
 
---
+++ src/pages/RaffleInfo.tsx
@@ -0,0 +1,12 @@
+import { Link } from "react-router-dom"
+
+function RaffleInfo() {
+    return (
+        <>
+            <h1>RaffleInfo</h1>
+            <Link to="/">Go Home</Link>
+        </>
+    )
+}
+
+export default RaffleInfo
